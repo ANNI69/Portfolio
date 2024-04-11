0cdcf6ca470233eac6c5c68487c382e2b63def50
Anii <aniruddh.nagare@vpt.edu.in>
2024-04-11 15:16:15 +0530

    Insights

--- Portfolio/portfolio/app/layout.tsx
+++ Portfolio/portfolio/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
